f934511973eab509bc831617db12d6bee7fedcb6
Zeitel42 <43343991+Zeitel42@users.noreply.github.com>
2022-02-28 12:09:48 -0500

    changed eventListener to submit added event.preventDefault

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
@@ -7,11 +7,13 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
 
+event.preventDefault();
+
 var listItemEl = document.createElement("li");
 listItemEl.className = "task-item";
 listItemEl.textContent = "This is a new task";
 
 tasksToDoEl.appendChild(listItemEl);
-
+console.log(event);
 };
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
